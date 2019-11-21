c2a2f55b6845803bf2c3f885ceaf9303b621675f
atsf8068 <andyh.2468@hotmail.com>
2019-11-20 17:05:31 -0800

    Simplefied customer gui panels

--- CPSC304-SuperRent/src/ca/ubc/cs304/ui/guiWindow.java
+++ CPSC304-SuperRent/src/ca/ubc/cs304/ui/guiWindow.java
@@ -12,23 +12,34 @@ public class guiWindow {
     private JButton clerkButton;
     private JButton makeReservationButton;
     private JButton vAvailVeh;
-    private JPanel vehicleSelection;
-    private JPanel selectType;
     private JComboBox vTypes;
-    private JPanel selectLocation;
+    private JPanel selectVehicles;
     private JComboBox selLocation;
-    private JPanel selectTime;
     private JTextField selTime;
-    private JButton stButton;
     private JButton backButtonSelType;
-    private JButton backButtonSelLoc;
-    private JButton backButtonSelTime;
-    private JButton okButtonVT;
+    private JPanel Reservation;
+    private JPanel isCustomer;
+    private JButton yesButtonIsCustomer;
+    private JButton noButtonIsCustomer;
+    private JPanel addCustomer;
+    private JTextField nameAddCustomer;
+    private JTextField addressAddCustomer;
+    private JTextField cellAddCustomer;
+    private JTextField licenseAddCustomer;
+    private JButton addCustomerButton;
+    private JButton backButtonAddCustomer;
+    private JButton backButtonIsCustomer;
+    private JPanel makeReservation;
+    private JComboBox selectTypeMakeReservation;
+    private JTextField licenseMakeReservation;
+    private JTextField fromDateMakeReservation;
+    private JTextField toDateMakeReservation;
+    private JButton makeReservationButton1;
+    private JButton backButtonMakeReservation;
+    private JButton showAvaliableVehiclesButton;
+    private JPanel vehicleSelection;
+    private JButton backToSelUser;
     private String vehicleType;
-    private String location;
-    private String city;
-    private String time;
-    private TerminalTransactions t;
 
 
     public guiWindow() {
@@ -47,56 +58,92 @@ public class guiWindow {
         vAvailVeh.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                switchPanel(selectLocation);
+                switchPanel(selectVehicles);
             }
         });
-        okButtonVT.addActionListener(new ActionListener() {
+        showAvaliableVehiclesButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 vehicleType = (String)vTypes.getSelectedItem();
                 if(vehicleType.equalsIgnoreCase("None")){
                     vehicleType = null;
                 }
-                switchPanel(selectTime);
+                String loc = (String)selLocation.getSelectedItem();
+                String time = selTime.getText();
+                //format location and time
+                //query data with transaction
+                //switchPanel(result panel);
             }
         });
-        selLocation.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                JComboBox cb = (JComboBox)actionEvent.getSource();
-                String locationCity = (String)cb.getSelectedItem();
-                //gets locationCity into fields
-                switchPanel(selectType);
-            }
-        });
-        stButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                String timeText = selTime.getText();
-                if(timeText.isEmpty()){
-                    //time values are null
-                }
-                //format time to get required information and put in fields
-                //call transaction method
-                //switch to panel that produces result
-            }
-        });
+
+
         backButtonSelType.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                switchPanel(selectLocation);
-            }
-        });
-        backButtonSelLoc.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 switchPanel(customerSelection);
             }
         });
-        backButtonSelTime.addActionListener(new ActionListener() {
+
+        makeReservationButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                switchPanel(selectType);
+                switchPanel(isCustomer);
+            }
+        });
+        noButtonIsCustomer.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                switchPanel(addCustomer);
+            }
+        });
+        addCustomerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                //uses information of textfields to create new customer tuple
+                String name = nameAddCustomer.getText();
+                String address = addressAddCustomer.getText();
+                String cellphone = cellAddCustomer.getText();
+                String driversLicense = licenseAddCustomer.getText();
+
+            }
+        });
+        backButtonAddCustomer.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                switchPanel(isCustomer);
+            }
+        });
+        backButtonIsCustomer.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                switchPanel(customerSelection);
+            }
+        });
+        yesButtonIsCustomer.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                switchPanel(makeReservation);
+            }
+        });
+        backButtonMakeReservation.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                switchPanel(isCustomer);
+            }
+        });
+        backToSelUser.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                switchPanel(users);
+            }
+        });
+        makeReservationButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                String vt = (String)selectTypeMakeReservation.getSelectedItem();
+                String dl = licenseMakeReservation.getText();
+                String fromDate = fromDateMakeReservation.getText();
+                String toDate = toDateMakeReservation.getText();
             }
         });
     }
@@ -116,5 +163,4 @@ public class guiWindow {
         panel1.repaint();
         panel1.revalidate();
     }
-
 }
