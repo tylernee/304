295fae2c78a5eaeb9f62c52c146680aa57fa2d50
Miguel Moya <miguelmoya97@hotmail.com>
2019-11-23 22:31:18 -0800

    removed terminaltranasction stuff in superrent

--- CPSC304-SuperRent/src/ca/ubc/cs304/controller/SuperRent.java
+++ CPSC304-SuperRent/src/ca/ubc/cs304/controller/SuperRent.java
@@ -2,7 +2,6 @@ package ca.ubc.cs304.controller;
 
 import ca.ubc.cs304.database.DatabaseConnectionHandler;
 import ca.ubc.cs304.delegates.LoginWindowDelegate;
-import ca.ubc.cs304.model.CustomerModel;
 import ca.ubc.cs304.ui.LoginWindow;
 import ca.ubc.cs304.ui.guiWindow;
 
@@ -50,48 +49,7 @@ public class SuperRent implements LoginWindowDelegate {
 //			transaction.showMainMenu(this);
 		}
 	}
-
-    public void showCustomers() {
-    	CustomerModel[] customers = dbHandler.getCustomers();
-    }
-
-    public String handleRent(int confNo, String cardName, int cardNo, String expDate) {
-    	String receipt = dbHandler.handleRent(confNo, cardName, cardNo, expDate);
-    	return "receipt";
-	}
-
-	public String handleRentNoReservation(String vtname, String cardName, int cardNo, String expDate) {
-//    	String receipt = dbHandler.handleRentNoReservation(vtname, cardName, cardNo, expDate);
-    	return "";
-	}
-
-	public void insertNewCustomer(CustomerModel customer) {
-    	dbHandler.insertNewCustomer(customer);
-	}
-	public void returnVehicle(int rid, String date, String time, int odometer, boolean fulltank) {
-    	dbHandler.returnVehicle(rid, date, time, odometer, fulltank);
-	}
-
-
-    /**
-	 * TerminalTransactionsDelegate Implementation
-	 * 
-     * The TerminalTransaction instance tells us that it is done with what it's 
-     * doing so we are cleaning up the connection since it's no longer needed.
-     */ 
-    public void terminalTransactionsFinished() {
-    	dbHandler.close();
-    	dbHandler = null;
-    	
-    	System.exit(0);
-    }
-
-
-    public void makeReservation(int dlicense) {
-//    	dbHandler.makeReservation(dlicense);
-	}
-
-
+	
     
 	/**
 	 * Main method called at launch time
